c430c6ad6df96c2cfcf03881b497968eb04cbaf0
Vawlpe <vawlpe@gmail.com>
2020-12-08 02:01:13 +0200

    Heroku stuff

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 from datetime import datetime
 import os
 
-TOKEN = open("token.txt").read()
+#TOKEN = open("token.txt").read() #LOCAL TESTING
+TOKEN = os.getenv('TOKEN')
 
 client = commands.Bot(
 	command_prefix = "vaw.",
