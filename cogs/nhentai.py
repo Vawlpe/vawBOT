ade3bcc6b9777005552b8f67ff6943a29a56e3e6
Vawlpe <vawlpe@gmail.com>
2020-12-09 06:11:50 +0200

    Switched to discord.ext.menus; working NHV & NHR

--- cogs/nhentai.py
+++ cogs/nhentai.py
@@ -1,94 +1,83 @@
 import discord
 from discord.ext import commands
+from discord.ext import menus
 import asyncio
 
 from hentai import Hentai, Format, Utils
 
-# Helper funcs
-# View Doujin
-async def viewDoujin(self, ctx, d):
-    # Create Embed...
-    embed=discord.Embed(color=0x850054, 
-        title=d.title(Format.Pretty),
-        url=d.url,
-        description=f"Artists: {', '.join(artist.name for artist in d.artist)}\nTags: {', '.join(tag.name for tag in d.tag)}"
-    )
-    embed.set_thumbnail(url='https://i.imgur.com/uLAimaY.png')
-    embed.set_image(url=d.cover)
-    embed.add_field(name='Read',value=':book: REACT BELLOW')
-    embed.add_field(name='Download',value=f'[:inbox_tray: Torrent](https://nhentai.net/login/?next=%2Fg%2F{d.id}%2Fdownload)')
-    embed.add_field(name='Favorite',value=f'[:star: {d.num_favorites}](https://nhentai.net/login/?next=%2Fg%2F{d.id}%2F)')
-    embed.set_footer(text=f'ID: {d.id} | {d.num_pages} Pages')
+# Menus
+class viewDoujinMenu(menus.Menu):
+    async def start(self, ctx, d):
+        self.d = d
+        await super().start(ctx)
 
-    #Send and add reaction button
-    msg = await ctx.send(embed=embed)
-    await msg.add_reaction("📖")
+    async def send_initial_message(self, ctx, channel):
+        # Create Embed...
+        embed=discord.Embed(color=0x850054, 
+            title=self.d.title(Format.Pretty),
+            url=self.d.url,
+            description=f"Artists: {', '.join(artist.name for artist in self.d.artist)}\nTags: {', '.join(tag.name for tag in self.d.tag)}"
+        )
+        embed.set_thumbnail(url='https://i.imgur.com/uLAimaY.png')
+        embed.set_image(url=self.d.cover)
+        embed.add_field(name='Read',value=':book: REACT BELLOW')
+        embed.add_field(name='Download',value=f'[:inbox_tray: Torrent](https://nhentai.net/login/?next=%2Fg%2F{self.d.id}%2Fdownload)')
+        embed.add_field(name='Favorite',value=f'[:star: {self.d.num_favorites}](https://nhentai.net/login/?next=%2Fg%2F{self.d.id}%2F)')
+        embed.set_footer(text=f'ID: {self.d.id} | {self.d.num_pages} Pages')
 
-    # Added reaction Check callback
-    def check(reaction, user):
-        isSameMsg = reaction.message == msg 
-        read = reaction.emoji == '📖'
-        notBot = reaction.count>1 and reaction.me
-        isSameUser = (usr async for usr in reaction.users() if usr == msg.author)
-        return isSameMsg and read and notBot and isSameUser
+        return await ctx.send(embed=embed)
 
-    # Wait for user to react and redirect them to NHREAD
-    try:
-        await self.client.wait_for('reaction_add', timeout=120.0, check=check)
-    except asyncio.TimeoutError:
-        print(f'NHVIEW {d.id} Read Reaction Timeout')
-        await msg.clear_reaction('📖')
-        await msg.add_reaction('⌛')
-    else:
-        print(f'NHVIEW {d.id} -> NHREAD')
-        await msg.clear_reaction('📖')
-        await ctx.send("Redirecting to NHREAD...")
-        await viewPage(self, ctx, d=d, msg=msg)
+    @menus.button('\N{OPEN BOOK}')
+    async def on_book_open(self, payload):
+        await viewPageMenu().start(self.ctx, self.d)
+        await self.message.delete()
+        self.stop()
 
-# View Page
-async def viewPage(self, ctx, d, page=0, msg=None):
-    # Create Embed...
-    embed=discord.Embed(color=0x850054, 
-        title=d.title(Format.Pretty),
-        url=d.url
-    )
-    embed.set_thumbnail(url='https://i.imgur.com/uLAimaY.png')
-    embed.set_image(url=d.image_urls[page])
-    embed.add_field(name='Download',value=f'[:inbox_tray: Torrent](https://nhentai.net/login/?next=%2Fg%2F{d.id}%2Fdownload)')
-    embed.add_field(name='Favorite',value=f'[:star: {d.num_favorites}](https://nhentai.net/login/?next=%2Fg%2F{d.id}%2F)')
-    embed.set_footer(text=f'ID: {d.id} | {page+1}/{d.num_pages} Pages')
+class viewPageMenu(menus.Menu):
+    async def start(self, ctx, d):
+        self.d=d
+        self.page=0
+        await super().start(ctx)
 
-    #Nav reactions
-    if msg is None:
-        msg = await ctx.send(embed=embed)
-    else:
-        await msg.edit(embed=embed)
+    async def domsg(self, ctx=None):
+        # Create Embed...
+        embed=discord.Embed(color=0x850054, 
+            title=self.d.title(Format.Pretty),
+            url=self.d.url
+        )
+        embed.set_thumbnail(url='https://i.imgur.com/uLAimaY.png') # NH Logo
+        embed.set_image(url=self.d.image_urls[self.page])
+        embed.add_field(name='Download',value=f'[:inbox_tray: Torrent](https://nhentai.net/login/?next=%2Fg%2F{self.d.id}%2Fdownload)')
+        embed.add_field(name='Favorite',value=f'[:star: {self.d.num_favorites}](https://nhentai.net/login/?next=%2Fg%2F{self.d.id}%2F)')
+        embed.set_footer(text=f'ID: {self.d.id} | {self.page+1}/{self.d.num_pages} Pages')
+        if self.message is None:
+            return await ctx.send(embed=embed)
+        else:
+            await self.message.edit(embed=embed)
 
-    await msg.clear_reaction("⏮️")
-    await msg.clear_reaction("⬅️")
-    await msg.clear_reaction("➡️")
-    await msg.clear_reaction("⏭️")
-    await msg.add_reaction("⏮️")
-    await msg.add_reaction("⬅️")
-    await msg.add_reaction("➡️")
-    await msg.add_reaction("⏭️")
+    async def send_initial_message(self, ctx, channel):
+        return await self.domsg(ctx)
 
-    async def start():
-        newpage=0
-        await viewPage(self, ctx, d=d, page=newpage, msg=msg) # Works
-    async def prev():
-        newpage=max(0, min(page-1, d.num_pages))
-        await viewPage(self, ctx, d=d, page=newpage, msg=msg) # Works once cuz using Page and using newpage before defined BLAHBLAHBLAH
-    async def next():
-        newpage=max(0, min(page+1, d.num_pages))
-        await viewPage(self, ctx, d=d, page=newpage, msg=msg) # Always goes to pg 1 for same reasons as above
-    async def end():
-        newpage=d.num_pages-1
-        await viewPage(self, ctx, d=d, page=newpage, msg=msg) # Works
-    await self.client.get_cog("UtilsCog").expectMsgReactions(msg=msg, reaction="⏮️", callback=start)
-    await self.client.get_cog("UtilsCog").expectMsgReactions(msg=msg, reaction="⬅️", callback=prev)
-    await self.client.get_cog("UtilsCog").expectMsgReactions(msg=msg, reaction="➡️", callback=next)
-    await self.client.get_cog("UtilsCog").expectMsgReactions(msg=msg, reaction="⏭️", callback=end)
+    @menus.button('⏮️')
+    async def on_start(self, payload):
+        self.page=0
+        await self.domsg()
+
+    @menus.button('⬅️')
+    async def on_prev(self, payload):
+        self.page=max(0, min(self.page-1, self.d.num_pages-1))
+        await self.domsg()
+
+    @menus.button('➡️')
+    async def on_next(self, payload):
+        self.page=max(0, min(self.page+1, self.d.num_pages-1))
+        await self.domsg()
+
+    @menus.button('⏭️')
+    async def on_end(self, payload):
+        self.page=self.d.num_pages-1
+        await self.domsg()
+
 
 
 # Cogs setup bs
@@ -105,12 +94,12 @@ class NHCog(commands.Cog):
         if Hentai.exists(id):
                 d = Hentai(id)
                 print(f'NHVIEW {id} found')
-                await viewDoujin(self, ctx, d)
+                await viewDoujinMenu().start(ctx, d)
         # No ID? Send random
         elif id is None:
             print(f'NHVIEW No ID; Sending random')
             await ctx.send('No ID passed, getting random doujin...')
-            await viewDoujin(self, ctx, Utils.get_random_hentai())
+            await viewDoujinMenu().start(ctx, Utils.get_random_hentai())
         # ID not digits
         elif not id.isdigit():
             print(f'NHVIEW Invalid ID')
@@ -123,7 +112,7 @@ class NHCog(commands.Cog):
     @commands.command()
     async def nhr(self, ctx, *, id, replaceMsg=None):
         print(f'NHREAD {id}...')
-        await viewPage(self, ctx, d=Hentai(id), msg=replaceMsg)
+        await viewPageMenu().start(ctx, d=Hentai(id))
 
 # More cogs setup bs
 def setup(client):
