0d1ca61fffd8f3805322ac246473e4bd00dac1fa
Vawlpe <vawlpe@gmail.com>
2021-01-25 14:41:59 +0200

    Chapter navigation for reader menu

--- cogs/webcomics.py
+++ cogs/webcomics.py
@@ -11,30 +11,38 @@ class WebcomicCommands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    async def twokinds_init(self, url, page):
+        article = BeautifulSoup(requests.get(url).text, 'html.parser').find('article')
+        total_pages = int(article.find(class_='navprev')['href'].replace('/','').replace('comic',''))+1
+        article = BeautifulSoup(requests.get(url.replace('comic','archive')).text, 'html.parser').find('article')
+        chapters= article.find_all(class_='chapter')
+    
+        chfpg = {}
+        for i, ch in enumerate(chapters):
+            chfpg[i]=int(ch.find(class_='chapter-links').find(class_='jsdep')['href'].replace('/','').replace('comic',''))
+
+        return {'totalPages':total_pages, 'chfpg':chfpg}
 
     async def twokinds_proc(self, url, page):
-    	twokinds_url = f'{url}comic/{page}' if page!=0 else url
+    	twokinds_url = f'{url}{page}' if page!=0 else url
     	article = BeautifulSoup(requests.get(twokinds_url).text, 'html.parser').find('article')
     	page_url = article.find('img')['src']
     	page_title = article.find('h1').text
 
-    	twokinds_url = url
-    	article = BeautifulSoup(requests.get(twokinds_url).text, 'html.parser').find('article')
-
-    	total_pages = int(article.find(class_='navprev')['href'].replace('/','').replace('comic',''))+1
-    	print(total_pages)
-    	return {'url':page_url, 'title':page_title, 'totalPages':total_pages, 'extra_fields':None,}
+    	return {'url':page_url, 'title':page_title, 'extra_fields':None,}
 
     @commands.command()
     async def twokinds(self, ctx, *, page=0):
     	return await readmenu.ReadMenu().start(ctx,
-    			url='https://twokinds.keenspot.com/',
+    			url='https://twokinds.keenspot.com/comic/',
     			color=0x72ceef,
     			thumbnail='https://i.imgur.com/YVMsQJ3.png',
     			page=page,
     			footerFormat='{other}{page}/{total_pages}',
-    			imgURLbase='https://twokinds.keenspot.com/',
-    			imgURLproc=self.twokinds_proc
+    			imgURLbase='https://twokinds.keenspot.com/comic/',
+                showbtns=[True,True,True,True,True,True],
+                init=self.twokinds_init,
+    			proc=self.twokinds_proc
     		)
 
 def setup(client):
--- menus/read.py
+++ menus/read.py
@@ -3,7 +3,7 @@ from discord.ext import menus
 import asyncio
 
 class ReadMenu(menus.Menu):
-    async def start(self, ctx, title=None, url=None, color=None, thumbnail=None, page=None, footerFormat=None, extra_fields=None, totalPages=None, imgURLs=None, imgURLbase=None, imgURLproc=None):
+    async def start(self, ctx, title=None, url=None, color=None, thumbnail=None, page=None, footerFormat=None, extra_fields=None, totalPages=None, imgURLs=None, imgURLbase=None, showbtns={True,False,True,True,False,True}, init=None, proc=None):
         self.title=title
         self.url=url
         self.color=color
@@ -14,7 +14,9 @@ class ReadMenu(menus.Menu):
         self.totalPages=totalPages
         self.imgURLs=imgURLs
         self.imgURLbase=imgURLbase
-        self.imgURLproc=imgURLproc
+        self.showbtns=showbtns
+        self.init=init
+        self.proc=proc
         await super().start(ctx)
 
     async def domsg(self, ctx=None):
@@ -31,12 +33,12 @@ class ReadMenu(menus.Menu):
             # Use URL list[page]
             embed.set_image(url=self.imgURLs[self.page-1])
             self.totalPages=len(imgURLs)
-        elif self.imgURLproc is None:
+        elif self.proc is None:
             # Use URL base/page
-            embed.set_image(url=f'{self.imgURLbase}comic/{self.page}')
+            embed.set_image(url=f'{self.imgURLbase}{self.page}')
         else:
-            # Preprocess URL base/page
-            processed=await self.imgURLproc(self.imgURLbase, self.page)
+            # Proc URL base/page
+            processed=await self.proc(self.imgURLbase, self.page)
             embed.set_image(url=processed['url'])
             if processed['title'] is not None:
                 embed.title=processed['title']
@@ -44,8 +46,10 @@ class ReadMenu(menus.Menu):
             if processed['extra_fields'] is not None:
                 self.extra_fields=processed['extra_fields']
 
-            if processed['totalPages'] is not None:
-                self.totalPages=processed['totalPages']
+            for i, ch in enumerate(self.chfpg):
+                if self.chfpg[i]>self.page:
+                    self.currch=i-1
+                    break
 
         if self.page==0:
                     self.page=self.totalPages
@@ -63,24 +67,60 @@ class ReadMenu(menus.Menu):
             await self.message.edit(embed=embed)
 
     async def send_initial_message(self, ctx, channel):
+        proc = await self.init(self.imgURLbase, self.page)
+
+        if proc['totalPages'] is not None:
+            self.totalPages=proc['totalPages']
+
+        if proc['chfpg'] is not None:
+            self.chfpg=proc['chfpg']
+
         return await self.domsg(ctx)
 
-    @menus.button('⏮️')
+    def show_start(self):
+        return not self.showbtns[0]
+
+    def show_prevch(self):
+        return not self.showbtns[1]
+
+    def show_prev(self):
+        return not self.showbtns[2]
+
+    def show_next(self):
+        return not self.showbtns[3]
+
+    def show_nextch(self):
+        return not self.showbtns[4]
+
+    def show_end(self):
+        return not self.showbtns[5]
+
+    @menus.button('⏮️',skip_if=show_start)
     async def on_start(self, payload):
         self.page=1
         await self.domsg()
 
-    @menus.button('⬅️')
+    @menus.button('⏪',skip_if=show_prevch)
+    async def on_prevch(self, payload):
+        self.page=max(1, min(self.chfpg[self.currch-1], self.totalPages))
+        await self.domsg()
+
+    @menus.button('⬅️',skip_if=show_prev)
     async def on_prev(self, payload):
         self.page=max(1, min(self.page-1, self.totalPages))
         await self.domsg()
 
-    @menus.button('➡️')
+    @menus.button('➡️',skip_if=show_next)
     async def on_next(self, payload):
         self.page=max(1, min(self.page+1, self.totalPages))
         await self.domsg()
 
-    @menus.button('⏭️')
+    @menus.button('⏩',skip_if=show_nextch)
+    async def on_nextch(self, payload):
+        self.page=max(1, min(self.chfpg[self.currch+1], self.totalPages))
+        await self.domsg()
+
+    @menus.button('⏭️',skip_if=show_end)
     async def on_end(self, payload):
         self.page=self.totalPages
         await self.domsg()
