9101b697f645178a9222a24886ec397e411e7936
Vawlpe <vawlpe@gmail.com>
2021-01-26 18:40:58 +0200

    Port nhv to view menu

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -7,43 +7,7 @@ import nekos
 from luscious_dl import album as lalbum
 
 import menus.read as readmenu
-
-# Menus
-class viewDoujinMenu(menus.Menu):
-    async def start(self, ctx, d):
-        self.d = d
-        await super().start(ctx)
-
-    async def send_initial_message(self, ctx, channel):
-        # Create Embed...
-        embed=discord.Embed(color=0xE12754, 
-            title=self.d.title(Format.Pretty),
-            url=self.d.url
-        )
-        embed.set_thumbnail(url='https://i.imgur.com/uLAimaY.png') # NH Logo
-        embed.set_image(url=self.d.cover)
-
-        if self.d.artist: embed.add_field(name='Artist',value=f"{', '.join(artist.name for artist in self.d.artist)}")
-        if self.d.language: embed.add_field(name='Language',value=f"{', '.join(lang.name for lang in self.d.language)}")
-        if self.d.group: embed.add_field(name='Group',value=f"{', '.join(group.name for group in self.d.group)}")
-        if self.d.parody: embed.add_field(name='Parody',value=f"{', '.join(parody.name for parody in self.d.parody)}")
-        if self.d.category: embed.add_field(name='Category',value=f"{', '.join(cat.name for cat in self.d.category)}")
-        if self.d.character: embed.add_field(name='Characters',value=f"{', '.join(char.name for char in self.d.character)}")
-        if self.d.tag: embed.add_field(name='Tags',value=f"{', '.join(tag.name for tag in self.d.tag)}",inline=False)
-
-        embed.add_field(name='Read',value=':book: React To Read')
-        embed.add_field(name='Download',value=f'[:inbox_tray: Torrent](https://nhentai.net/login/?next=%2Fg%2F{self.d.id}%2Fdownload)')
-        embed.add_field(name='Favorite',value=f'[:star: {self.d.num_favorites}](https://nhentai.net/login/?next=%2Fg%2F{self.d.id}%2F)')
-        embed.set_footer(text=f'ID: {self.d.id} | {self.d.num_pages} Pages')
-
-        return await ctx.send(embed=embed)
-
-    @menus.button('\N{OPEN BOOK}')
-    async def on_book_open(self, payload):
-        await viewPageMenu().start(self.ctx, self.d)
-        await self.message.delete()
-        self.stop()
-
+import menus.view as viewmenu
 
 class viewDoujinListMenu(menus.Menu):
     async def start(self, ctx, doujinList):
@@ -118,29 +82,6 @@ class HentaiCommands(commands.Cog):
         if not ctx.channel.is_nsfw(): await ctx.channel.send("NSFW Commands can only run in NSFW channels.")
         return ctx.channel.is_nsfw()
 
-    # NHView
-    @commands.command()
-    async def nhv(self, ctx, *, id=None):
-        print(f'NHView {id}...')
-
-        # Doujin exists?
-        if Hentai.exists(id):
-                d = Hentai(id)
-                print(f'NHVIEW {id} found')
-                await viewDoujinMenu().start(ctx, d)
-        # No ID? Send random
-        elif id is None:
-            print(f'NHVIEW No ID; Sending random')
-            await ctx.send('No ID passed, getting random doujin...')
-            await viewDoujinMenu().start(ctx, Utils.get_random_hentai())
-        # ID not digits
-        elif not id.isdigit():
-            print(f'NHVIEW Invalid ID')
-            await ctx.send('Invalid ID\nIDs are usually 6 digit numbers, although there are some 5 digin and even shorter or longer IDs\nIf you don\'t have an ID just don\'t write one and we will send you a random doujin :3')
-        else:
-            print(f'NHVIEW {id} not found ;-;')
-            await ctx.send(f'No Doujin with id: {id} was found\nIf you don\'t have an ID just don\'t write one and we will send you a random doujin :3')
-
     # NHRead
     @commands.command()
     async def nhr(self, ctx, *, id=None):
@@ -176,11 +117,60 @@ class HentaiCommands(commands.Cog):
                 {'name':'Favorite','value':f'[:star: {d.num_favorites}](https://nhentai.net/login/?next=%2Fg%2F{d.id}%2F)'}
             ],
             'imgURLs':d.image_urls,
-            'showbtns':[True,False,True,True,False,True]
+            'showbtns':[True,False,True]*2
         }
 
         return await readmenu.ReadMenu().start(ctx, **info)
 
+
+    # NHView
+    @commands.command()
+    async def nhv(self, ctx, *, id=None):
+        print(f'NHView {id}...')
+
+        # No ID? Send random
+        if id is None:
+            print(f'NHVIEW No ID; Sending random')
+            await ctx.send('No ID passed, getting random doujin...')
+            d=Utils.get_random_hentai()
+        # ID not digits
+        elif not id.isdigit():
+            print(f'NHVIEW Invalid ID')
+            await ctx.send('Invalid ID\nIDs are usually 6 digit numbers, although there are some 5 digin and even shorter or longer IDs\nIf you don\'t have an ID just don\'t write one and we will send you a random doujin :3')
+        # Doujin exists?
+        elif Hentai.exists(id):
+                d = Hentai(id)
+                print(f'NHVIEW {id} found')
+        else:
+            print(f'NHVIEW {id} not found ;-;')
+            await ctx.send(f'No Doujin with id: {id} was found\nIf you don\'t have an ID just don\'t write one and we will send you a random doujin :3')
+
+        info={
+            'title':d.title(Format.Pretty),
+            'url':d.url,
+            'color':0xE12754,
+            'thumbnail':'https://i.imgur.com/uLAimaY.png',
+            'cover':d.cover,
+            'footerExtra':f'ID: {d.id} | {d.num_pages} Pages',
+            'extra_fields':[
+                {'name':'Artist','value':f"{', '.join(artist.name for artist in d.artist)}"} if d.artist else None,
+                {'name':'Language','value':f"{', '.join(lang.name for lang in d.language)}"} if d.language else None,
+                {'name':'Group','value':f"{', '.join(group.name for group in d.group)}"} if d.group else None,
+                {'name':'Parody','value':f"{', '.join(parody.name for parody in d.parody)}"} if d.parody else None,
+                {'name':'Category','value':f"{', '.join(cat.name for cat in d.category)}"} if d.category else None,
+                {'name':'Characters','value':f"{', '.join(char.name for char in d.character)}"} if d.character else None,
+                {'name':'Tags','value':f"{', '.join(tag.name for tag in d.tag)}",'inline':False} if d.tag else None,
+
+                {'name':'Read','value':':book: React To Read'},
+                {'name':'Download','value':f'[:inbox_tray: Torrent](https://nhentai.net/login/?next=%2Fg%2F{d.id}%2Fdownload)'},
+                {'name':'Favorite','value':f'[:star: {d.num_favorites}](https://nhentai.net/login/?next=%2Fg%2F{d.id}%2F)'}
+            ],
+            'showreadbtn': True,
+            'read':self.nhr,
+            'readargs': {'ctx':ctx, 'id':d.id}
+        }
+
+        return await viewmenu.ViewMenu().start(ctx, **info)
 
     # NekosLife
     @commands.command()
